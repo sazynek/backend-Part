c71a77e882a78eed194e13fd4b9b612b1ae38281
ALEKSANDR <alesandr2021@gmail.com>
2024-12-10 20:44:54 +0500

    refresh packge.json(jwt)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
-import {Module} from "@nestjs/common";
+import { Module } from '@nestjs/common'
+import { UserModule } from './user/user.module';
+import { ArticlesModule } from './articles/articles.module';
+import { CommentsModule } from './comments/comments.module';
+import { ProductsModule } from './products/products.module';
+import { StaticticModule } from './statictic/statictic.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
-  exports: []
+	imports: [UserModule, ArticlesModule, CommentsModule, ProductsModule, StaticticModule, AuthModule],
+	controllers: [],
+	providers: [],
+	exports: [],
 })
 export class AppModule {}
