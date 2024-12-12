9c96fa85e8d45f7d579561e25f219c3bc3ee4936
ALEKSANDR <alesandr2021@gmail.com>
2024-12-12 21:47:56 +0500

    fix prisma,add enum-prisma

--- src/enums/Status.ts
+++ src/enums/Status.ts
@@ -1,5 +1,13 @@
-enum Status {
-	'Healthy',
-	'Trending',
-	'Supreme',
+export enum EnumStatus {
+	'healthy',
+	'trending',
+	'supreme',
+}
+export enum EnumPraiseStatus {
+	'premium',
+	'base',
+}
+export enum EnumProductCategories {
+	'chicken',
+	'chicken_with_vegetables',
 }
